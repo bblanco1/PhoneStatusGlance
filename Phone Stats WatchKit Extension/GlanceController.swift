a75c83efdce428e9d9ae18dc00785335a85d7d7f
Thomas Elliott <tom.w.elliott@gmail.com>
2016-06-13 16:25:10 +0100

    Add activity indicator and separate error display (with retry) to glance.

--- Phone Stats WatchKit Extension/GlanceController.swift
+++ Phone Stats WatchKit Extension/GlanceController.swift
@@ -14,11 +14,26 @@ class GlanceController: WKInterfaceController, WCSessionDelegate {
     // Our WatchConnectivity Session for communicating with the iOS app
     var watchSession : WCSession?
     
+    var hasData: Bool = false
+    var hasError: Bool = false
+    
+    var active: Bool = false
+    
     @IBOutlet var signalLabel: WKInterfaceLabel?
     @IBOutlet var batteryLabel: WKInterfaceLabel?
     
+    @IBOutlet var loadingGroup: WKInterfaceGroup?
+    @IBOutlet var infoGroup: WKInterfaceGroup?
+    
+    @IBOutlet var iconImage: WKInterfaceImage?
+    
+    @IBOutlet var loadingLabel: WKInterfaceLabel?
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
+        
+        hasData = false
+        hasError = false
         
         // Configure interface objects here.
         if(WCSession.isSupported()){
@@ -28,28 +43,78 @@ class GlanceController: WKInterfaceController, WCSessionDelegate {
             watchSession!.activateSession()
         }
     }
-
+    
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        loadData()
+        active = true;
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+        active = false
+    }
+    
+    func setLoading(isLoading: Bool){
+        if isLoading {
+            self.iconImage?.setImageNamed("Activity")
+            self.iconImage?.startAnimatingWithImagesInRange(NSMakeRange(0,15), duration: 1.0, repeatCount: 0)
+        } else {
+            self.iconImage?.stopAnimating()
+            self.iconImage?.setImageNamed("icon-transparent-50x50")
+        }
+        
+        if hasError {
+            loadingGroup?.setHidden(false)
+            infoGroup?.setHidden(true)
+            return
+        }
+        
+        loadingLabel?.setText("Loading")
+        
+        if hasData {
+            loadingGroup?.setHidden(true)
+            infoGroup?.setHidden(false)
+        } else {
+            loadingGroup?.setHidden(!isLoading)
+            infoGroup?.setHidden(isLoading)
+        }
+        
+    }
 
+    func loadData(){
+        setLoading(true)
         watchSession?.sendMessage(["request" : "update"], replyHandler: { (response: [String : AnyObject]) in
             let battery : String = response["battery"] as! String
             self.batteryLabel?.setText(battery)
             let signal : String = response["signal"] as! String
             self.signalLabel?.setText(signal)
             
-            self.batteryLabel?.setText("80%")
-            
+            self.setLoading(false)
+            self.hasData = true
+            self.hasError = false
             }, errorHandler: { (error: NSError) in
-                self.batteryLabel?.setText("Error!")
+                print(error.description)
+                self.hasError = true
+                self.loadingLabel?.setText("Error. Will retry shortly")
+                self.delay(3.0, closure: { 
+                    if self.active {
+                        self.loadData()
+                    }
+                })
             }
         )
     }
     
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
+    func delay(delay:Double, closure:()->()) {
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(delay * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(), closure)
     }
-
+    
 }
