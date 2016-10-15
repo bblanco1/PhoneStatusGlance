f7191719e6b458a396a32f69f79b31cf69f38c89
Thomas Elliott <tom.w.elliott@gmail.com>
2016-10-15 11:49:03 -0400

    Update travis script to allow beta and release deployment.

--- fastlane/travis.sh
+++ fastlane/travis.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
 
+# If a pull request, only ever test
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+  bundle exec fastlane test
+  exit $?
+fi
+
+# Beta deploy
+if [[ "$TRAVIS_BRANCH" == "beta" ]]; then
+  bundle exec fastlane getmatch && bundle exec fastlane beta
+  exit $?
+fi
+
+# Release for review
+if [[ "$TRAVIS_BRANCH" == "release" ]]; then
+  bundle exec fastlane getmatch && bundle exec fastlane deploy
+  exit $?
+fi
+
+# Default, just run the tests
 bundle exec fastlane test
 exit $?
